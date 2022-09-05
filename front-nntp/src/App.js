02cecebbb94b7cb09edb103a9a37916297a0c1fe
Marceli Koćwin <988marceli@gmail.com>
2022-09-05 23:37:29 +0200

    react simple example

--- front-nntp/src/App.js
+++ front-nntp/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Marceli(props) {
+  return <h1>{props.name.title}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Marceli name={{title:"Koza to super kolega, ale nie zna się na żartach"}}></Marceli>
         <a
           className="App-link"
           href="https://reactjs.org"
